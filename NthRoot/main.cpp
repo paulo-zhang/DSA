4fe5e52ed885b859b21d263fd79e5e99d377725f
polar.zhang <zhyuanshan@gmail.com>
2021-10-10 13:28:06 +0800

    Nth Root

---
+++ NthRoot/main.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+#define e 0.000001
+template<unsigned int N>
+double NthRoot(double d, double lower, double upper){
+    double mid = lower + (upper - lower) / 2;
+    double multiply = mid;
+    for(int i = N; i > 1; i --){
+        multiply *= mid;
+    }
+
+    double diff = multiply - d;
+    
+    if(abs(diff) <= e){
+        return mid;
+    }
+
+    if(diff > e){
+        return NthRoot<N>(d, lower, mid);
+    }
+    else {
+        return NthRoot<N>(d, mid, upper);
+    }
+}
+
+double cubicRoot(double d){
+    return NthRoot<3>(d, 1, d);
+}
+
+double squareRoot(double d){
+    return NthRoot<2>(d, 1, d);
+}
+
+int main()
+{
+    double d = 8;
+    cout << d << "cubicRoot: " << cubicRoot(d) << "squareRoot: " << squareRoot(d);
+    return 0;
+}
