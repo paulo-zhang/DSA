a6de658a32085afc6f735b47eb2ba4913d7c1b74
paulo-zhang <zhyuanshan@gmail.com>
2021-11-25 13:37:04 +0800

    Longest Common Substring, Optimized.

--- LongestCommonSubstring/main.cpp
+++ LongestCommonSubstring/main.cpp
@@ -5,27 +5,17 @@
 using namespace std;
 // https://www.geeksforgeeks.org/longest-common-substring-dp-29/
 
-int LCSubStr_Recursive(const string &s1, int index1, const string &s2, int index2, bool &mark)
+int LCSubStr_Recursive(const string &s1, int index1, const string &s2, int index2, int count)
 {
     if (s1.size() <= index1 || s2.size() <= index2)
-        return 0;
+        return count;
 
     if (s1[index1] == s2[index2])
     {
-        mark = true;
-        bool consecutive = false;
-        int ret = LCSubStr_Recursive(s1, index1 + 1, s2, index2 + 1, consecutive);
-        if (consecutive)
-        {
-            return ret + 1;
-        }
-
-        return 1;
+        return LCSubStr_Recursive(s1, index1 + 1, s2, index2 + 1, count + 1);
     }
 
-    mark = false;
-    bool m1 = false, m2 = false;
-    return max(LCSubStr_Recursive(s1, index1 + 1, s2, index2, m1), LCSubStr_Recursive(s1, index1, s2, index2 + 1, m2));
+    return max(count, max(LCSubStr_Recursive(s1, index1 + 1, s2, index2, 0), LCSubStr_Recursive(s1, index1, s2, index2 + 1, 0)));
 }
 
 int LCSubStr_DP(const string &s1, const string &s2)
@@ -55,14 +45,13 @@ int LCSubStr_DP(const string &s1, const string &s2)
 
 int main(int, char **)
 {
-    vector<string> ss1 = {"GeeksforGeeks", "abcdxyz", "zxabcdezy"};
-    vector<string> ss2 = {"GeeksQuiz", "xyzabcd", "yzabcdezx"};
+    vector<string> ss1 = {"GeeksforGeeks", "abcdxyz", "zxabcdezy", "Site:GeeksforGeeks"};
+    vector<string> ss2 = {"GeeksQuiz", "xyzabcd", "yzabcdezx", "Site:GeeksQuiz"};
 
     for (int i = 0; i < ss1.size(); ++i)
     {
-        bool mark;
         cout << "Length of Longest Common Substring of [" << ss1[i] << "] and [" << ss2[i] << "] is:\n"
-             << "Recursive: " << LCSubStr_Recursive(ss1[i], 0, ss2[i], 0, mark) << "\n"
+             << "Recursive: " << LCSubStr_Recursive(ss1[i], 0, ss2[i], 0, 0) << "\n"
              << "DP: " << LCSubStr_DP(ss1[i], ss2[i]) << "\n";
     }
 }
