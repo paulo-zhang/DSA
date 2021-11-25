f93952107a7582509bec745b419492c4f5db3f11
paulo-zhang <zhyuanshan@gmail.com>
2021-11-25 12:13:09 +0800

    Longest Common Substring. Optimized.

--- LongestCommonSubstring/main.cpp
+++ LongestCommonSubstring/main.cpp
@@ -30,26 +30,27 @@ int LCSubStr_Recursive(const string &s1, int index1, const string &s2, int index
 
 int LCSubStr_DP(const string &s1, const string &s2)
 {
-    vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
-    for (int i = 1; i <= s1.size(); ++i)
+    vector<vector<int>> dp(2, vector<int>(s2.size() + 1, 0));
+    int i;
+    for (i = 1; i <= s1.size(); ++i)
     {
         for (int j = 1; j <= s2.size(); ++j)
         {
             if (s1[i - 1] == s2[j - 1])
             {
                 if (i > 1 && j > 1 && s1[i - 2] == s2[j - 2])
-                    dp[i][j] = dp[i - 1][j - 1] + 1; // Consecutive
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1; // Consecutive
                 else
-                    dp[i][j] = max(dp[i - 1][j - 1], 1);
+                    dp[i % 2][j] = max(dp[(i - 1) % 2][j - 1], 1);
             }
             else
             {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
             }
         }
     }
 
-    return dp[s1.size()][s2.size()];
+    return dp[i % 2][s2.size()];
 }
 
 int main(int, char **)
