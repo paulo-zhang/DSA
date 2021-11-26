8aba2f0092490576ff50f3535c5d870963d6caef
paulo-zhang <zhyuanshan@gmail.com>
2021-11-26 14:23:26 +0800

    The longest norepeat substring.

---
+++ LongestNorepeatSubString/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+// https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+
+int longestUniqueSubsttr(string input)
+{
+    unordered_map<char, int> visitedIndex;
+    int maxCount = 0, currCount = 0;
+    string theString;
+
+    for (int i = 0; i < input.size(); ++i)
+    {
+        if (visitedIndex.find(input[i]) != visitedIndex.end())
+        {
+            i = visitedIndex[input[i]]; // Move to next index, start over again.
+            visitedIndex.clear();
+            currCount = 0;
+        }
+        else
+        {
+            currCount++;
+            if (currCount > maxCount)
+            {
+                maxCount = max(maxCount, currCount);
+                theString = input.substr(i + 1 - currCount, currCount);
+            }
+
+            visitedIndex[input[i]] = i;
+        }
+    }
+
+    cout << theString << "\n";
+    return maxCount;
+}
+
+int main(int, char **)
+{
+    string str = "geeksforgeeks";
+    cout << "The input string is " << str << endl;
+    int len = longestUniqueSubsttr(str);
+    cout << "The length of the longest non-repeating "
+            "character substring is "
+         << len << "\n";
+    return 0;
+}
