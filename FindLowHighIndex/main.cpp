5026f90e1bf8bb7a90c4be73a9c464c15f63e96a
polar.zhang <zhyuanshan@gmail.com>
2022-04-16 12:36:07 +0800

    Clearer code.

--- FindLowHighIndex/main.cpp
+++ FindLowHighIndex/main.cpp
@@ -8,50 +8,42 @@ using namespace std;
 /* if x is present in arr[] then returns the index of
 FIRST occurrence of x in arr[0..n-1], otherwise
 returns -1 */
-int lower(int arr[], int low, int high, int x)
-{
-  if(low > high) return low;
+pair<int, int> searchRange(int arr[], int n, int x) {
+        if(n == 0) return pair<int, int>{-1, -1}; // 1. Check empty.
+        
+        int low = 0, high = n - 1;
+        while(low <= high){ // 2. Use '<='.
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] >= x){ // 3. Use '>=', only move low when low is not pointing to the answer to make sure low will point to lower bound.
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        if(low >= n || arr[low] != x){ // 4. low could overflow if target is not found.
+            return pair<int, int>{-1, -1};
+        }
+        
+        int first = low; // 5. Save the first result.
+        high = n - 1; // 6. Set the new search boundary, low is not changed.
+        while(low <= high){ // Also use '<='.
+            int mid = (low + high) / 2;
+            
+            if(arr[mid] > x){ // Use '>' (the only difference with previous while loop), only move high when high is not pointing to the answer to make sure high will point to the last target value.
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        return pair<int, int>{first, high};
+    }
 
-  int mid = (high + low) / 2;
-
-  if (arr[mid] < x)
-    return lower(arr, mid + 1, high, x);
-  else
-    return lower(arr, low, mid - 1, x);
-}
-
-/* if x is present in arr[] then returns the index of
-LAST occurrence of x in arr[0..n-1], otherwise
-returns -1 */
-int upper(int arr[], int low, int high, int x)
-{
-  if(low > high) return low; // low will be finally pointing to the next index of last x.
-  
-  int mid = (high + low) / 2;
-	
-  if(arr[mid] <= x)
-    return upper(arr, mid + 1, high, x);
-  else
-    return upper(arr, low, mid - 1, x);
-}
-
-void findFirstAndLast(int arr[], int n, int x)
-{
-	int first, last;
-	// to store first occurrence
-	first = lower(arr, 0, n - 1, x);
-	// to store last occurrence
-	last = upper(arr, 0, n - 1, x) - 1;
-
-	if (first > last) {
-		first = -1;
-		last = -1;
-	}
-
-	cout << "First: " << first << ", Last: " << last << "\n";
-}
-
-void std_LowUpper(int arr[], int n, int x)
+pair<int, int> std_LowUpper(int arr[], int n, int x)
 {
 	int first, last;
 	// to store first occurrence
@@ -59,12 +51,16 @@ void std_LowUpper(int arr[], int n, int x)
 	// to store last occurrence
 	last = upper_bound(arr, arr + n, x) - arr - 1;
 
-	if (first > last) {
+	if (first > last) { 
 		first = -1;
 		last = -1;
 	}
 
-	cout << "First: " << first << ", Last: " << last << "\n";
+	return pair<int, int>{first, last};
+}
+
+ostream& operator <<(ostream& os, const pair<int, int>& p){
+	return os << "first: " << p.first << ", second: " << p.second << "\n";
 }
 
 // Driver program
@@ -75,28 +71,28 @@ int main()
 
 	int x = 8;
 	cout << "val: " << x << "\n";
-	findFirstAndLast(arr, n, x);
-	std_LowUpper(arr, n, x);
+	cout << searchRange(arr, n, x);
+	cout << std_LowUpper(arr, n, x);
 
 	x = 2;
 	cout << "val: " << x << "\n";
-	findFirstAndLast(arr, n, x);
-	std_LowUpper(arr, n, x);
+	cout << searchRange(arr, n, x);
+	cout << std_LowUpper(arr, n, x);
 
 	x = 5;
 	cout << "val: " << x << "\n";
-	findFirstAndLast(arr, n, x);
-	std_LowUpper(arr, n, x);
+	cout << searchRange(arr, n, x);
+	cout << std_LowUpper(arr, n, x);
 
 	x = 9;
 	cout << "val: " << x << "\n";
-	findFirstAndLast(arr, n, x);
-	std_LowUpper(arr, n, x);
+	cout << searchRange(arr, n, x);
+	cout << std_LowUpper(arr, n, x);
 
 	x = 0;
 	cout << "val: " << x << "\n";
-	findFirstAndLast(arr, n, x);
-	std_LowUpper(arr, n, x);
+	cout << searchRange(arr, n, x);
+	cout << std_LowUpper(arr, n, x);
 
 	return 0;
 }
